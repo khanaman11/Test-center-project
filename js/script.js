4587b759fcf4f9f5ba96ce72a071b6d2bb2e79ff
Aman khan <amanullahkhan11595@gmail.com>
2024-10-26 13:09:10 +0530

    replace code

--- js/script.js
+++ js/script.js
@@ -8,21 +8,4 @@ function showImage(imageIndex) {
             img.classList.add('active');
         }
     });
-}
-
-
-// JavaScript for Toggle Card Selection and Custom Message Box .
-
-function toggleCardSelection(cardNumber) {
-    const card = document.querySelector(`#checkbox${cardNumber}`).closest('.option-card');
-    const checkbox = document.getElementById(`checkbox${cardNumber}`);
-
-    // Toggle the selected state of the card and checkbox
-    card.classList.toggle('selected');
-    checkbox.checked = !checkbox.checked;
-}
-
-function toggleMessageBox() {
-    const messageBox = document.querySelector('.custom-message');
-    messageBox.style.display = messageBox.style.display === 'none' ? 'block' : 'none';
-}
+};
